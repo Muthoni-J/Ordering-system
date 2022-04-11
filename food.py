fdf10f9a24fc93b328b4bef0e402f6c7459383e0
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-04-11 18:28:05 +0300

    first commit

---
+++ food.py
@@ -0,0 +1,30 @@
+#Menu loop
+
+#Print Main Menu
+print("Welcome to McJoans")
+order = input("Can I take yor order?")
+print("1: Pizza Large  Medium Small ")
+print("2: Burger Large  Medium Small ")
+print("3: Fries Large  Medium Small ")
+print("4: Chicken Wings Cruchy Spicy ")
+print("5: Drink Juice Soda  Coffee ")
+print("6: Exit")
+
+#Get menu selection from user
+selection =input("Enter Selection (1-4): ")
+
+#Take Action Based on Menu Selection
+if selection == "1" :
+    print("Option 1")
+elif selection ==  "2" :
+    print("Option 2")
+if selection == "3" :
+    print("Option 3")
+elif selection ==  "4" :
+    print("Option 4")
+if selection == "5" :
+    print("Option 5")
+elif selection ==  "6" :
+    print("Exit")   
+
+print("Thank you for chossing McJoans . Goodbye")     
